2f66db8431ca5e8e3797a046bbab819aec6c5532
haryoiro <55312590+haryoiro@users.noreply.github.com>
2021-02-26 22:50:11 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,10 @@ const yargs = require('yargs/yargs')
 const Parser = require('./Parser')
 const Code = require('./Code')
 
+const argv = yargs(process.argv.slice(2))
+  .check((a: any) => {
+    if (a._.length > 1) throw new Error('Only 0 or 1 files may be passed.');
+    return true
+  }).argv
 
-async function main() {
-  const argv = yargs(process.argv.slice(2))
-    .check((a: any) => {
-      const filePath = a._
-      if (filePath.length > 1) throw new Error('Only 0 or 1 files may be passed.');
-      else return true
-    }).argv
-  const path = argv._
-
-  new Parser(path[0])
-}
-
-main()
-
+new Parser(argv._[0])
